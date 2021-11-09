3e4325de9230505cdd100142a07c6c71f77f6ad3
jamal <carldeltoro@gmail.com>
2021-11-09 14:43:05 -0400

    convencion de nombres en test

--- src/test/java/com/curso/testing/util/StringUtilTest.java
+++ src/test/java/com/curso/testing/util/StringUtilTest.java
@@ -35,4 +35,24 @@ public class StringUtilTest
             throw new RuntimeException(actual + "is not equal to " + expected);
         }
     }*/
+
+    @Test
+    public void repeat_string_once() {
+        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("hola", -1);
+    }
 }
